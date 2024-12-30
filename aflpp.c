4cf1efb3b55425edaf10f6e4dff42208a95848b1
Darmenov Alisher <115714693+Anatomik5@users.noreply.github.com>
2024-12-30 23:57:37 +0500

    Add files via upload

--- aflpp.c
+++ aflpp.c
@@ -1,5 +1,12 @@
 #include "afl-fuzz.h"
 #include "afl-mutations.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h> 
+#include <string.h>
+#include <stdbool.h>
+
 
 typedef struct my_mutator {
 
@@ -37,15 +44,13 @@ my_mutator_t *afl_custom_init(afl_state_t *afl, unsigned int seed) {
   return data;
 
 }
-const size_t start_offset = 0xd0;
-const size_t end_offset = 0x170;
+const size_t start_offset = 0x10030;
+
 
 size_t afl_custom_fuzz(my_mutator_t *data, uint8_t *buf, size_t buf_size,
                        u8 **out_buf, uint8_t *add_buf, size_t add_buf_size,
                        size_t max_size) {
-
-  size_t segment_length = end_offset - start_offset;
-
+                  
   // Check if max_size exceeds data->buf_size and reallocate if necessary
   if (max_size > data->buf_size) {
     u8 *ptr = realloc(data->buf, max_size);
@@ -56,25 +61,22 @@ size_t afl_custom_fuzz(my_mutator_t *data, uint8_t *buf, size_t buf_size,
       data->buf_size = max_size;
     }
   }
-
   u32 havoc_steps = 1 + rand_below(data->afl, 16);
-
+     
   memcpy(data->buf, buf, buf_size);
-
-  u8 *temp_buf = malloc(segment_length);
-  if (!temp_buf) {
-    printf("temp_buf");
-    return 0;
+  size_t end_offset = start_offset+0x4;;
+  size_t i = start_offset;
+  while(i+0x3 < buf_size){
+     if (data->buf[i+2] == 0x5f && data->buf[i+3] == 0xd6 &&
+        data->buf[i] == 0xc0 && data->buf[i+1] == 0x03) {
+        end_offset = i;
+        break;
+      }
+      i=i+0x4;
   }
-  memcpy(temp_buf, buf + start_offset, segment_length);
-
-  u32 mutated_segment_len = afl_mutate(data->afl, temp_buf, segment_length, 
-                                       havoc_steps, false, true, add_buf, add_buf_size, segment_length);
-
-
-  memcpy(data->buf + start_offset, temp_buf, mutated_segment_len);
-
-  free(temp_buf);
+  size_t segment_length= end_offset - start_offset;
+  u32 mutated_segment_len = afl_mutate(data->afl, data->buf + start_offset, segment_length, 
+                                       havoc_steps, false, true, add_buf, add_buf_size, max_size);
 
   *out_buf = data->buf;
   return buf_size;
